7bc7dff1a293ce1263be3bbf765a9183ae5f87c1
Luca Gaibazzi <lgaibazz@c2r6p1.42firenze.it>
2024-12-03 17:16:30 +0100

    First commit.

---
+++ server/server.js
@@ -0,0 +1,12 @@
+// server/server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Hello from the server!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
